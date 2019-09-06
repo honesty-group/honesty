750820ba523367239c44a7e9b9bee818796c6e4d
IceHacks <38994479+IceHacks@users.noreply.github.com>
2019-09-05 15:23:10 -1000

    Build base for profiles

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+/**
+ * (c) 2019 Honesty Group
+ *
+ * This script is the core of Honesty.
+ */
+
+const _ = require("lodash");
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 
+// Database
 const db = {
 	profiles: low(new FileSync(__dirname + "/storage/profiles.json"))
 };
@@ -10,3 +18,21 @@ db.profiles
 		data: []
 	})
 	.write();
+
+// Functions
+const profile = ({ name, email, phone, id }) => {
+	let prof = db.profiles.get("profiles");
+	let user = name
+		? prof.find({ name }).value()
+		: email
+		? prof.find({ email }).value()
+		: phone
+		? prof.find({ phone }).value()
+		: id
+		? prof.find({ id }).value()
+		: false;
+
+	console.log(user);
+};
+
+profile({ name: "IceHacks" });
